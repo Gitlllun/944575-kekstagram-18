346d3a48a274be8557e5203bdfb003b2ed50d3a0
lllun <l.l.lun@ya.ru>
2019-10-26 21:47:46 +0300

    add big picture

--- js/main.js
+++ js/main.js
@@ -1,54 +1,55 @@
 'use strict';
 
-var NUMBER_POSTS = 25;
+// 5. ЛИЧНЫЙ ПРОЕКТ: ПОКА ВСЕ ДОМА
+
+var USER_NAMES = ['Артём', 'Маша', 'Игорь', 'Коля', 'Настя'];
+var USER_POSTS = 25;
+var USER_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var LIKES_MIN = 15;
+var LIKES_MAX = 200;
 var POST_DESCRIPTION = ['Дорогие друзья, выбранный нами инновационный путь влечет за собой процесс', 'Не следует, однако, забывать о том, что новая модель организационной', 'Равным образом дальнейшее развитие различных форм деятельности требует от нас', 'Таким образом, начало повседневной работы по формированию позиции требует от'];
-var LIKE_MIN = 15;
-var LIKE_MAX = 200;
-var POST_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var AVATAR_MIN = 1;
 var AVATAR_MAX = 6;
-var POST_NAMES = ['Артём', 'Маша', 'Игорь', 'Коля', 'Настя'];
 
-// Функция генерация массива с нужным количеством данных
-var createNumber = function (x, y) {
+// Генерация рандомного значения
+var getRandomElement = function (arr) {
+  return Math.floor(Math.random() * arr.length);
+};
+
+// Генерация массива с нужным количеством данных
+var createNumber = function (min, max) {
   var number = [];
 
-  for (var i = x; i <= y; i++) {
+  for (var i = min; i <= max; i++) {
     number.push(i);
   }
 
   return number;
 };
 
-// Функция получения рандомного значения
-var getRandomElement = function (arr) {
-  return Math.floor(Math.random() * arr.length);
-};
-
-// Функция для создания массива из сгенерированных объектов для описания фотографии, опубликованной пользователем
-var createPost = function (number) {
+// Массив из сгенерированных объектов для описания фотографии
+var createPost = function (value) {
   var post = [];
 
-  var POST_LIKES = createNumber(LIKE_MIN, LIKE_MAX);
+  var POST_LIKES = createNumber(LIKES_MIN, LIKES_MAX);
   var POST_AVATARS = createNumber(AVATAR_MIN, AVATAR_MAX);
 
-  for (var i = 1; i <= number; i++) {
+  for (var i = 1; i <= value; i++) {
     var urlPost = 'photos/' + i + '.jpg';
     var descriptionPost = getRandomElement(POST_DESCRIPTION);
     var likesPost = getRandomElement(POST_LIKES);
     var randomAvatars = getRandomElement(POST_AVATARS);
-    var commentsPost = getRandomElement(POST_COMMENTS);
-    var namesPost = getRandomElement(POST_NAMES);
-    var avatarsPost = 'img/avatar-' + randomAvatars + '.svg';
+    var commentsPost = getRandomElement(USER_COMMENTS);
+    var namesPost = getRandomElement(USER_NAMES);
 
     post.push({
       url: urlPost,
       description: POST_DESCRIPTION[descriptionPost],
       likes: POST_LIKES[likesPost],
       comments: [{
-        avatar: avatarsPost,
-        message: getRandomElement(POST_COMMENTS[commentsPost]),
-        name: POST_NAMES[namesPost]
+        avatar: 'img/avatar-' + POST_AVATARS[randomAvatars] + '.svg',
+        message: USER_COMMENTS[commentsPost],
+        name: USER_NAMES[namesPost]
       }]
     });
   }
@@ -58,8 +59,9 @@ var createPost = function (number) {
 
 var pictureTemplate = document.querySelector('#picture')
   .content
-  .querySelector('a');
+  .querySelector('.picture');
 
+// Заполнение данных поста
 var renderPicture = function (obj) {
   var picture = pictureTemplate.cloneNode(true);
 
@@ -70,6 +72,7 @@ var renderPicture = function (obj) {
   return picture;
 };
 
+// Генерация и отрисовка постов
 var picturePost = document.querySelector('.pictures');
 
 var renderPost = function (post) {
@@ -82,135 +85,56 @@ var renderPost = function (post) {
   return picturePost.appendChild(fragment);
 };
 
-renderPost(createPost(NUMBER_POSTS));
+renderPost(createPost(USER_POSTS));
 
-// Начало работы с ТЗ
+// 6. ЛИЧНЫЙ ПРОЕКТ: БОЛЬШЕ ДЕТАЛЕЙ
 
-var uploadFile = document.querySelector('#upload-file');
-var uploadOverlay = document.querySelector('.img-upload__overlay');
-var uploadClosed = uploadOverlay.querySelector('#upload-cancel');
+var FIRST_POST = 1;
 
-var ESC_KEYCODE = 27;
-var ENTER_KEYCODE = 13;
+var bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
 
-var onOverlayEscPress = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    closeOverlay();
+// Прячет блок счетчика комментариев и загрузки новых
+bigPicture.querySelector('.comments-loader').classList.add('visually-hidden');
+bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden');
+
+var preview = bigPicture.querySelector('.big-picture__preview');
+var listComments = bigPicture.querySelector('.social__comments');
+var itemComment = bigPicture.querySelector('.social__comment');
+
+// Удаление встроенных комментариев 
+var deleteComments = function () {
+  var children = listComments.children
+  
+  for (var i = children.length - 1; i >= 0; i--) {
+    children[i].parentElement.removeChild(children[i]);
   }
 };
 
-var openOverlay = function () {
-  uploadOverlay.classList.remove('hidden');
-  document.addEventListener('keydown', onOverlayEscPress);
+// Заполнение данных поста в полноэкранном режиме
+var renderBigPicture = function (obj) {
+  var picture = preview.cloneNode(true);
+
+  picture.querySelector('.big-picture__img img').src = obj.url;
+  picture.querySelector('.likes-count').textContent = obj.likes;
+  picture.querySelector('.comments-count').textContent = obj.comments.length;
+  picture.querySelector('.social__caption').textContent = obj.description;
+  picture.querySelector('.social__picture').alt = obj.comments[0].name;
+  picture.querySelector('.social__picture').src = obj.comments[0].avatar;
+  picture.querySelector('.social__text').textContent = obj.comments[0].message;
+
+  return picture;
 };
 
-var closeOverlay = function () {
-  uploadOverlay.classList.add('hidden');
-  document.removeEventListener('keydown', onOverlayEscPress);
+// Генерация и отрисовка поста в полноэкранном режиме
+var renderBigPost = function (post) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < post.length; i++) {
+    fragment.appendChild(renderBigPicture(post[i]));
+  }
+  
+  return bigPicture.replaceChild(fragment, preview);
 };
 
-// Открывает форму редактирования изображения
-uploadFile.addEventListener('change', function (evt) {
-  evt.preventDefault();
-
-  document.querySelector('.img-upload__form').reset();
-  uploadFile.value = '';
-  controlValue.value = '100%';
-  imagePreview.style.transform = 'scale(1)';
-  imagePreview.className = '';
-  document.querySelector('.img-upload__effect-level').classList.add('hidden');
-
-  openOverlay();
-});
-
-uploadFile.addEventListener('change', function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    evt.preventDefault();
-    openOverlay();
-  }
-});
-
-// Закрывает форму редактирования изображения
-uploadClosed.addEventListener('click', function (evt) {
-  evt.preventDefault();
-
-  closeOverlay();
-});
-
-uploadClosed.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_KEYCODE) {
-    evt.preventDefault();
-    closeOverlay();
-  }
-});
-
-// Переменные для масштабирования
-var SCALE_MAX = 100;
-var SCALE_MIN = 25;
-var SCALE_STEP = 25;
-var FLAG_MINUS = -1;
-var FLAG_PLUS = 1;
-
-var sectionPicture = document.querySelector('.pictures');
-var imagePreview = sectionPicture.querySelector('.img-upload__preview img');
-var scalePreview = sectionPicture.querySelector('.img-upload__scale');
-var controlSmaller = scalePreview.querySelector('.scale__control--smaller');
-var controlValue = scalePreview.querySelector('.scale__control--value');
-var controlBigger = scalePreview.querySelector('.scale__control--bigger');
-
-// Функция расчета масштабирования
-var getScale = function (number) {
-  var parseValue = parseFloat(controlValue.value);
-  var totalValue = parseValue + number * SCALE_STEP;
-
-  if (totalValue > SCALE_MAX) {
-    totalValue = SCALE_MAX;
-  } else if (totalValue < SCALE_MIN) {
-    totalValue = SCALE_MIN;
-  }
-
-  controlValue.value = totalValue + '%';
-  imagePreview.style.transform = 'scale(' + (totalValue / SCALE_MAX) + ')';
-};
-
-// Уменьшение масштаба
-controlSmaller.addEventListener('click', function (evt) {
-  evt.preventDefault();
-
-  getScale(FLAG_MINUS);
-});
-
-// Увеличение масштаба
-controlBigger.addEventListener('click', function (evt) {
-  evt.preventDefault();
-
-  getScale(FLAG_PLUS);
-});
-
-// Переменные доя фильтров
-var previewClasses = ['effects__preview--none', 'effects__preview--chrome', 'effects__preview--sepia', 'effects__preview--marvin', 'effects__preview--phobos', 'effects__preview--heat'];
-
-var IMAGE_EFFECT = ['effect-none', 'effect-chrome', 'effect-sepia', 'effect-marvin', 'effect-phobos', 'effect-heat'];
-
-var imageUploadEffect = document.querySelector('.img-upload__effects');
-
-var getImageEffect = function (evt) {
-  var target = evt.target.parentNode;
-
-  for (var i = 0; i < IMAGE_EFFECT.length; i++) {
-    if (target.previousElementSibling && target.previousElementSibling.id === IMAGE_EFFECT[i]) {
-      if (target.previousElementSibling.id === IMAGE_EFFECT[0]) {
-        document.querySelector('.img-upload__effect-level').classList.add('hidden');
-      } else {
-        document.querySelector('.img-upload__effect-level').classList.remove('hidden');
-      }
-
-      imagePreview.className = '';
-      imagePreview.classList.add(previewClasses[i]);
-    }
-  }
-};
-
-imageUploadEffect.addEventListener('click', function (evt) {
-  getImageEffect(evt);
-});
+renderBigPost(createPost(FIRST_POST));
